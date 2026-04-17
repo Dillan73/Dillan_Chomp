9fc63a47d2e0b0f675ef7f8e0ce9e8da27629daa
Dillan73 <dilly.agrawal@gmail.com>
2026-04-17 11:30:26 -0400

    Added comments to the working hashtable version

--- src/HashtableChomp_CURRENT.java
+++ src/HashtableChomp_CURRENT.java
@@ -3,12 +3,15 @@ import java.util.Arrays;
 import java.util.Hashtable;
 
 public class HashtableChomp_CURRENT {
-    int size; //i made it work for nxn instead of 10x10, but only use 10x10
-    ArrayList<int[]> allBoards; //this is where I store all the possible boards
-    private Hashtable<String, String> losingTable = new Hashtable<>();; // the losing boards. Uses a hashtable for efficiency. Done as strings bc .equals doesnt work for arrays.
-    long currentTime; // this is to help me see how long different parts of my project were taking
-    //takes ~0.5 seconds to make and no noticeable lag to play moves
+    int size; //i used this to be able to test more things (as a way to help debug and test ideas on simpler scenarios)
+    ArrayList<int[]> allBoards; //all the possible size by size boards
+    private Hashtable<String, String> losingTable = new Hashtable<>();; // the losing boards.
+        // I used a hashtable because I know its more efficient at retrieval. I switched to strings from arrays cuz I looked thru the hashtable docs and saw it uses .equals, which doesn't work for arrays
+    long currentTime; // used this to test how long specific parts/methods took
+        //takes ~0.6 seconds to make at the start and doesn't noticeably lag when playing a move
     public ArrayList<int[]> moves = new ArrayList<>(); // this stores the best move for a specific board
+        //i might change this to a hashtable that maps a board string to a move string.
+            //i think that would be more efficient for recall, but since recall is fast, im not prioritizing this
 
     public static void main(String[] args) {
         HashtableChomp_CURRENT doCoolThings = new HashtableChomp_CURRENT(10);
@@ -17,10 +20,10 @@ public class HashtableChomp_CURRENT {
     public HashtableChomp_CURRENT(int size){
         this.size = size;
 
-        //find the possible boards and store in allBoards
+        //this creates allBoards with all the possible boards
             findBoards();
 
-        //find all the best moves and store in moves
+        //this creates moves (aka finds the best move for each board)
             findBestMoves();
 
         System.out.println(losingTable.size());
@@ -31,7 +34,7 @@ public class HashtableChomp_CURRENT {
         System.out.println("Find the boards took " + timeTaken + " ms. Finding the best move for each board...");
 
         for(int[] arr : allBoards){
-            findMove(arr); // goes thru each board in order it was made
+            findMove(arr); // goes thru each board in order it was made (this way its always already checked if a possible move is losing
         }
 
         timeTaken = System.currentTimeMillis()-currentTime;
@@ -39,33 +42,35 @@ public class HashtableChomp_CURRENT {
     }
 
     private void findMove(int[] arr){
-        String concat = "";
+        String concat = ""; //this is how i
         concat = Arrays.toString(arr);
         int[] forMoves = new int[size+2]; //make the array that will be filled in w the board and best move
         System.arraycopy(arr, 0, forMoves, 0, size); //put the board in the forMoves array
 
-        for(int c = 0; c < size; c++){ //goes thru each column
-            for(int r = 0; r < arr[c]; r++){ // goes thru each possible move in the column
-                int[] postMove = Arrays.copyOf(arr, size);//the board if this move was played
+        for(int c = 0; c < size; c++){ //the nested loop checks every chip (aka every possible move)
+            for(int r = 0; r < arr[c]; r++){
+                //for each move, it finds what the board would be if it was played
+                int[] postMove = Arrays.copyOf(arr, size);
                 for(int curr = c; curr < size; curr++){
                     postMove[curr] = Math.min(postMove[curr], r);
                 } //makes the board if this move was played
-                String move = Arrays.toString(postMove); //converts to a string
+                String move = Arrays.toString(postMove); //converts the board of the move as a string
 
-                //if this move brings to a losing board, we will store the move as the move to play
+                //if ive already identified the board from this move as a losing board, this is the move i want to play
                 if(losingTable.get(move) != null){
                     forMoves[size] = r;
                     forMoves[size+1] = c;
                     moves.add(forMoves);
-                    return;
+                    return; //since ive found a winning move, theres no reason to do anything more
                 }
             }
         }
+        //this part runs if none of the moves were losing boards --> this board is a losing board
 
-        //if none of the moves were losing boards, add this board as a losing board
+        //makes the board as a string
         losingTable.put(concat, "losing");
 
-        //pick a move to play that keeps a lot of chips
+        //find a move to play that keeps a lot of chips
         for(int col = size-1; col >= 0; col--){
             for(int row = arr[col]-1; row >= 0; row--){
                 forMoves[size] = row;
